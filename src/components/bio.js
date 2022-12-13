0c6ba46fe96fec2c6b906fa0e2ff915d24bab8d0
calebthoward <55468777+calebthoward@users.noreply.github.com>
2022-12-13 14:51:41 -0500

    Bio \n

--- src/components/bio.js
+++ src/components/bio.js
@@ -44,7 +44,8 @@ const Bio = () => {
       />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
+          Written by <strong>{author.name}</strong>. 
+          <\n> {author?.summary || null}
         </p>
       )}
     </div>
