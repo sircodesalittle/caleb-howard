d0c85e13e01072d0b029975e0bfac3c6131b5236
Alex Dykstra <alex@alexdykstra.com>
2022-12-13 10:57:57 -0500

    add site header

---
+++ src/components/siteHeader.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+const SiteHeader = () => {
+  return (
+    <div className="bio">
+      <p>
+        <i>
+          Hey fill this in when you get a chance, Caleb. This is the area where
+          you can put info about your site that shows up only on this page.
+        </i>
+      </p>
+    </div>
+  )
+}
+
+export default SiteHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import SiteHeader from "../components/siteHeader"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
@@ -24,7 +25,7 @@ const BlogIndex = ({ data, location }) => {
 
   return (
     <Layout location={location} title={siteTitle}>
-      <Bio />
+      <SiteHeader />
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
