e47105da5e96d7c6c64abf3e3c6de499513240d6
calebthoward <55468777+calebthoward@users.noreply.github.com>
2023-01-20 14:46:55 -0500

    Profile pic, header favicon

--- src/components/siteHeader.js
+++ src/components/siteHeader.js
@@ -3,6 +3,16 @@ import * as React from "react"
 const SiteHeader = () => {
   return (
     <div className="bio">
+      <StaticImage
+        className="bio-avatar"
+        layout="fixed"
+        formats={["auto", "webp", "avif"]}
+        src="../images/favicon.png"
+        width={50}
+        height={50}
+        quality={95}
+        alt="Profile picture"
+      />
       <p>
         <i>
           The most rewarding life is one surrendered in love for something worthy of the sacrifice.
